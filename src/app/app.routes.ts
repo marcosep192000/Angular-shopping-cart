e4a6261b015f863d13c4398973f7a15ab03144fd
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2024-06-24 00:30:35 -0300

    :+1: refactor : conponent nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+   /*  {path: 'nac', component: },   */  
+    
+];
---
+++ src/app/shared/dasboard/fastAcces/fast-acces/fast-acces.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fast-acces',
+  standalone: true,
+  imports: [],
+  templateUrl: './fast-acces.component.html',
+  styleUrl: './fast-acces.component.css'
+})
+export class FastAccesComponent {
+
+}
