d4e83f9d42a477b3c1ee8a6a68c695c7eb9ce92c
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2024-07-13 23:14:33 -0300

    :+1: feat : create service product

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
 import { environments } from '../../environments/environments.prod';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../interfaces/Product';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService  {
+  form: any;
 
-constructor() { }
+constructor(private http : HttpClient ) { }
 
   base: String = environments.baseURL; 
 
+
+  getProducts():Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.base}supermarket`);
+  }
   
+
+  /* return this.http.get<Product[]>(`${this.base}/supermarket/create-supermarket`); */
 }
